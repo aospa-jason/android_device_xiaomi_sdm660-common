71e5bb11dfa93a2f06a01cad7bda2f349db50463
Marve1ous <104805389+babybitter@users.noreply.github.com>
2023-11-22 23:09:13 +0800

    sdm660-common: Abandon lineage livedisplay

--- sdm660.mk
+++ sdm660.mk
@@ -279,11 +279,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.light@2.0-service.xiaomi_sdm660
 
-# LiveDisplay native
-PRODUCT_PACKAGES += \
-    vendor.lineage.livedisplay@2.0-service-sdm \
-    vendor.lineage.livedisplay@2.0-service-sysfs
-
 # Media
 PRODUCT_PACKAGES += \
     android.hardware.media.c2@1.1.vendor
